c42c8733beb6af30dbca25f74b8074be0f68a4fb
dany <dacr63@gmail.com>
2021-08-01 00:38:54 +0200

    countdown timer

---
+++ script.js
@@ -0,0 +1,40 @@
+// Change this to change countdown target.
+const newYears = '8 Jan 2022';
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time) {
+    if (time < 10) {
+        return (`0${time}`)
+    } else {
+        return time
+    }
+    // return time < 10 ? (`0${time}`) : time;
+}
+
+countdown();
+setInterval(countdown, 1000);
+
+
+
