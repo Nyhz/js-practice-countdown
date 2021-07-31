5d912ea5a07357747524c9c1f9a01bd09eb78306
dany <dacr63@gmail.com>
2021-08-01 00:43:47 +0200

    improved variable names

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Change this to change countdown target.
-const newYears = '8 Jan 2022';
+const countdownTarget = '1 Jan 2022';
 
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
@@ -7,10 +7,10 @@ const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
 function countdown() {
-    const newYearsDate = new Date(newYears);
+    const targetDate = new Date(countdownTarget);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const totalSeconds = (targetDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
